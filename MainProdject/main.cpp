411a52ac4ad3143c52fd2fbb03eb2e9b23188ff6
unknown <luniakey@gmail.com>
2022-12-23 21:07:42 +0300

    Добавлены структуры для хранения данных

--- MainProdject/main.cpp
+++ MainProdject/main.cpp
@@ -7,6 +7,10 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #9. Phone_Talk\n";
     cout << "Author: Arina Paniznik\n";
+<<<<<<< HEAD
     cout << endl;
+=======
+    cout<<endl;
+>>>>>>> eb32a7f4292a16076fef86d41a33f1660f3a37ea
     return 0;
 }
